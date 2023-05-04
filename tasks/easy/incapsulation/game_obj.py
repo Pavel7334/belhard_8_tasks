15350157aee7cdac672cc59509b49f2c91873ebd
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-05-04 20:36:19 +0400

    completed the task game_obj

--- tasks/easy/incapsulation/game_obj.py
+++ tasks/easy/incapsulation/game_obj.py
@@ -8,3 +8,23 @@
 Координаты должны быть доступны для чтения (сделать property), а их изменение
 должно происходить в методе move(delta_x, delta_y). (изменение - это +=)
 """
+
+
+class GameObject:
+
+    def __init__(self, x: int, y: int) -> None:
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self) -> int:
+        return self.__x
+
+    @property
+    def y(self) -> int:
+        return self.__y
+
+    def move(self, x: int, y: int) -> tuple[int, int]:
+        self.__x += x
+        self.__y += y
+        return self.__x, self.__y
