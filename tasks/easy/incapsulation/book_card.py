7c95f4c8d8febb76bb9b337ae0eeda467c3bb502
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-05-04 14:01:16 +0400

    completed the task book_card

--- tasks/easy/incapsulation/book_card.py
+++ tasks/easy/incapsulation/book_card.py
@@ -15,3 +15,64 @@
 from datetime import date
 
 CURRENT_YEAR = date.today().year
+
+
+class BookCard:
+
+    def __init__(self, author: str, title: str, year: int) -> None:
+        self.author = author
+        self.title = title
+        self.year = year
+
+    def __eq__(self, other) -> bool:
+        return self.__year == other.__year
+
+    def __ne__(self, other) -> bool:
+        return self.__year != other.__year
+
+    def __lt__(self, other) -> bool:
+        return self.__year < other.__year
+
+    def __gt__(self, other) -> bool:
+        return self.__year > other.__year
+
+    def __le__(self, other) -> bool:
+        return self.__year <= other.__year
+
+    def __ge__(self, other) -> bool:
+        return self.__year >= other.__year
+
+    @property
+    def author(self) -> str:
+        return self.__author
+
+    @author.setter
+    def author(self, x: str) -> None:
+        if not isinstance(x, str):
+            raise ValueError
+        self.__author = x
+
+    @property
+    def title(self) -> str:
+        return self.__title
+
+    @title.setter
+    def title(self, x: str) -> None:
+        if not isinstance(x, str):
+            raise ValueError
+        self.__title = x
+
+    @property
+    def year(self) -> int:
+        return self.__year
+
+    @year.setter
+    def year(self, x: int) -> None:
+        if not isinstance(x, int):
+            raise ValueError
+        if x > CURRENT_YEAR:
+            raise ValueError
+        if x <= 0:
+            raise ValueError
+        self.__year = x
+
